fdf9761886d5d9bc2095de80db105851442ea4b0
Sergei Boreiko <hsvpass@gmail.com>
2023-01-12 22:15:25 +0100

    add binary search

---
+++ binarySearch.js
@@ -0,0 +1,25 @@
+
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+const binarySearch = (array,item) => {
+  let start = 0;
+  let middle ;
+  let end = arr.length;
+  let position = -1;
+  let found = false;
+
+  while (found === false && start <= end) { 
+    middle = Math.floor( (start + end) / 2 );
+    if (item === array[middle]) {
+      found = true;
+      position = middle;
+    }  
+      if (item < array[middle]) {
+        end = middle - 1;
+      }else{ 
+        start = middle + 1
+      }  
+  }
+  return position;
+}
+
+console.log (binarySearch(arr,4))
